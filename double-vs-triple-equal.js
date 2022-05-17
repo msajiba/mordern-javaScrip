459808b46416fb9fb54118d5479e980378b8841e
sajib-ahmed <onlineclass82@gmail.com>
2022-05-17 15:20:02 +0600

    double vs triple equal concept

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,14 @@
+/*  
+    == only value is equal then result true,
+    === value and type are equal then result true or false
+*/
+
+const first = 20;
+const second = '20';
+
+if (first === second){
+    console.log('condition is true');
+}
+else {
+    console.log("condition is false ");
+}
---
+++ null-vs-undefine.js
@@ -0,0 +1,41 @@
+/*
+1. let age ;
+console.log(age);
+*/
+
+
+
+/*
+function add (num1,num2){
+    console.log(num1,num2);
+    // return na kora  num1,num2;
+}
+const result = add(10,20);
+console.log(result);
+*/
+
+
+/*
+function add (num1,num2){
+    console.log(num1,num2);
+    return num1,num2;
+}
+const result = add (10);  //all parameter pass na kora
+console.log(result);
+
+*/
+
+
+/*
+const info = {name:'sajib', age: 10, };
+console.log(info.gf);  // object er property na thaka
+*/
+
+
+/*
+let fun = undefined;  // reserve key tai use kora jabe na
+console.log(fun);
+*/
+
+
+// null ==== khali.kono value nai..ja developer ta set kore dey 
