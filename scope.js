fc8dbb45505aca3ff2a928829c09f8b15664bbf4
sajib-ahmed <onlineclass82@gmail.com>
2022-05-17 16:26:21 +0600

    global and block scope

---
+++ scope.js
@@ -0,0 +1,14 @@
+let round = "this is let variable ";
+round = "change let variable";
+
+function add (num1, num2 ){
+    let result = num1 + num2;
+    if (result> 40){
+         var mood = 'happy';
+        console.log(mood);
+    }
+    console.log(mood);
+    
+}
+
+add(10,40);
