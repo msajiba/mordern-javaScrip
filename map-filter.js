c38eb994a902a43f693cc80c0216c88326dfdc89
sajib-ahmed <onlineclass82@gmail.com>
2022-05-17 15:43:24 +0600

    map and filter and find concept clear

---
+++ map-filter.js
@@ -0,0 +1,17 @@
+const numbers = [3,4,5,6,7,8];
+
+
+//map ===========================>
+
+const output =numbers.map(x=> x*x)
+console.log(output);  // full array dibe
+
+
+//filter ===========================>
+
+const result = numbers.filter(x => x>5);
+console.log(result);  // condition er modde full array dibe.
+
+//find ===========================>
+const search = numbers.find(x => x>5); 
+console.log(search); // condition er modde only ekta number dibe
