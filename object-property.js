d9a6d553a0c7c520ae4cda50796aaf38f4525b54
sajib-ahmed <onlineclass82@gmail.com>
2022-05-17 16:01:32 +0600

    restructure in object-property

--- object-property.js
+++ object-property.js
@@ -7,19 +7,22 @@ const students = [
     {id: 06, name: 'shuvo'}
 ];
 
-// let student = []; 
+// use for loop
+let student = []; 
+for (let i = 0; i < students.length; i++) {
+    const element = students[i];
+    student.push(element.name);
+}
 
-// for (let i = 0; i < students.length; i++) {
-//     const element = students[i];
-//     student.push(element.name);
-// }
+//use map
+const names = students.map(s => s.name);
+const ids = students.map (s =>s.id);
+console.log(names, ids);
 
-
-// const names = students.map(s => s.name);
-// const ids = students.map (s =>s.id);
-
-// console.log(names, ids);
-
-
-const sId = students.find(s => s.id > 3);
+//use filter
+const sId = students.filter(s => s.id > 3);
 console.log(sId);
+
+//use find
+const sIds = students.filter(s => s.id > 3);
+console.log(sIds);
